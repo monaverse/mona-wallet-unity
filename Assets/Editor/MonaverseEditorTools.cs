7c71bdba7bb6dad4882addca91d1f40e9737f8e7
Carlos Revelo <crevelop@gmail.com>
2024-05-17 19:44:50 +0200

    build: export tool for API package (#31)

--- Assets/Editor/MonaverseEditorTools.cs
+++ Assets/Editor/MonaverseEditorTools.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
@@ -6,8 +7,14 @@ namespace Monaverse.Editor
 {
     public static class MonaverseEditorTools
     {
-        [MenuItem("Tools/Monaverse/Export Package")]
+        [MenuItem("Tools/Monaverse/Export Packages")]
         public static void ExportUnityPackage()
+        {
+            ExportSDKPackage();
+            ExportAPIPackage();
+        }
+
+        private static void ExportSDKPackage()
         {
             // Base folders to include
             var baseFolders = new[]
@@ -22,19 +29,46 @@ namespace Monaverse.Editor
             {
                 "Assets/Monaverse/Core/Plugins/Mona/com.monaverse.api/Tests"
             };
+            
+            ExportPackage(baseFolders: baseFolders,
+                excludeFolders: excludeFolders,
+                packageName: "Monaverse");
+        }
 
-            // Get all assets, excluding the specified sub-folders
+        private static void ExportAPIPackage()
+        {
+            // Base folders to include
+            var baseFolders = new[]
+            {
+                "Assets/Monaverse/Core/Plugins/Mona/com.monaverse.api",
+                "Assets/Monaverse/Core/Plugins/Nethereum",
+                "Assets/Monaverse/Core/Plugins/Newtonsoft 3.0.2",
+            };
+
+            // Sub-folders to exclude
+            var excludeFolders = new []
+            {
+                "Assets/Monaverse/Core/Plugins/Mona/com.monaverse.api/Tests"
+            };
+            
+            ExportPackage(baseFolders: baseFolders,
+                excludeFolders: excludeFolders,
+                packageName: "com.monaverse.api");
+        }
+
+        private static void ExportPackage(IEnumerable<string> baseFolders, IEnumerable<string> excludeFolders, string packageName)
+        {
             var includePaths = baseFolders.SelectMany(folder =>
                     Directory.GetFiles(folder, "*", SearchOption.AllDirectories)
                         .Where(path => !excludeFolders.Any(path.StartsWith))
                         .Select(path => path.Replace("\\", "/")) // Normalize path for Unity
             ).ToArray();
 
-            const string packagePath = "Monaverse.unitypackage";
+            var packagePath = $"{packageName}.unitypackage";
             AssetDatabase.ExportPackage(includePaths, packagePath,
                 ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
 
-            UnityEngine.Debug.Log("Package exported successfully to: " + packagePath);
+            UnityEngine.Debug.Log($"{packagePath} package exported successfully");
         }
     }
 }
