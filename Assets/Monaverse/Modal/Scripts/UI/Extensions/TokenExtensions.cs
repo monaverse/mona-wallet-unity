69e79cb7daa90445bfe42967c763a3ed66b5fd08
Carlos <crevelop@gmail.com>
2024-07-01 16:37:33 +0200

    fix: avoid exception during token filtering

--- Assets/Monaverse/Modal/Scripts/UI/Extensions/TokenExtensions.cs
+++ Assets/Monaverse/Modal/Scripts/UI/Extensions/TokenExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Monaverse.Api.Modules.User.Dtos;
 using Monaverse.Core;
@@ -8,13 +9,24 @@ namespace Monaverse.Modal.UI.Extensions
     public static class TokenExtensions
     {
         public static bool CanBeImported(this TokenDto token)
-            =>  MonaverseModal.Instance.TokenFilter == null || MonaverseModal.Instance.TokenFilter(token);
-        
+            => MonaverseModal.Instance.TokenFilter == null || MonaverseModal.Instance.TokenFilter(token);
+
         public static List<TokenDto> GetFilteredTokens(this List<TokenDto> tokens)
             => MonaverseModal.Instance.TokenFilter == null
                 ? tokens
-                : tokens.FindAll(i=> MonaverseModal.Instance.TokenFilter(i));
-        
+                : tokens.FindAll(i =>
+                {
+                    try
+                    {
+                        return MonaverseModal.Instance.TokenFilter(i);
+                    }
+                    catch (Exception e)
+                    {
+                        MonaDebug.LogError($"failed to filter {i?.Name} token: {e.Message}");
+                        return false;
+                    }
+                });
+
         public static MonaRemoteSprite GetImageRemoteSprite(this TokenDto token)
         {
             var tokenImageUrl = token?.Image;
@@ -24,7 +36,7 @@ namespace Monaverse.Modal.UI.Extensions
                 MonaDebug.LogWarning($"Token {token?.Name} image url is null or empty. Using default image.");
                 tokenImageUrl = MonaConstants.Media.MonaRemoteLogo;
             }
-            
+
             return MonaRemoteSpriteFactory.GetRemoteSprite(tokenImageUrl);
         }
     }
